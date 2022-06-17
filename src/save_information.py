6d4f51c35c391bb50d3839ccb21b7a864ea27a03
AlbertoVf <avf.git-repos@protonmail.com>
2022-06-17 13:42:14 +0200

    docs: Add docs
    Generate docs, add requirements.

--- src/save_information.py
+++ src/save_information.py
@@ -2,16 +2,15 @@ import os
 
 from src.data import Data
 
-path_files = 'Primos2'
+path_files = 'Primos'
 csv_file = os.path.join(path_files, 'Informacion.csv')
 
 
-def write_primes(file, data) -> None:
+def write_primes(file, data):
     """
-    Agrega los numeros number_of_primes localizados al f, separandolos por espacios
-    :param file: Nombre del archivo
-    :param data: Lista de numeros number_of_primes
-    :return:
+    Write primes to file
+    :param file: File to write
+    :param data: Primes to write
     """
     file = os.path.join(path_files, file)
 
@@ -22,9 +21,8 @@ def write_primes(file, data) -> None:
 
 def export_information(data: Data):
     """
-    Agrega una nueva linea a un fichero csv con informacion sobre el range de number_of_primes calculado
-    :param data: Array con los data que se escribiran en el csv
-    :return:
+    Export information to csv file
+    :param data: Information to export
     """
     with open(csv_file, "a") as file:
         file.write(f"{data.__str__()}\n")
